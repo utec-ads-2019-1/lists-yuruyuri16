d48aa1ad9aee64caeebbee78c5d33a4016f3605a
miguelyurivilca <miguel_y_r@hotmail.com>
2019-04-13 21:21:30 -0500

    forward list almost finished

--- forward.h
+++ forward.h
@@ -3,75 +3,230 @@
 
 #include "list.h"
 #include "iterators/forward_iterator.h"
+#include <stdexcept>
+#include <iostream>
 
 template <typename T>
-class ForwardList : public List<T> {
-    public:
-        ForwardList() : List<T>() {}
+class ForwardList : public List<T>
+{
+public:
+    ForwardList() : List<T>() {}
 
-        T front() {
-            // TODO
-        }
+    T front()
+    {
+        return this->head->data;
+    }
 
-        T back() {
-            // TODO
-        }
+    T back()
+    {
+        return this->tail->data;
+    }
 
-        void push_front(T value) {
-            // TODO
-        }
+    void push_front(T value)
+    {
+        Node<T> *newNode;
 
-        void push_back(T value) {
-            // TODO
+        if (empty())
+        {
+            newNode = new Node<T>{value, nullptr, nullptr};
+            this->head = newNode;
+            this->tail = newNode;
         }
+        else
+        {
+            newNode = new Node<T>{value, this->head, nullptr};
+            this->head->prev = newNode;
+            this->head = newNode;
+        }
+        this->nodes++;
+    }
 
-        void pop_front() {
-            // TODO
-        }
+    void push_back(T value)
+    {
+        Node<T> *newNode;
 
-        void pop_back() {
-            // TODO
-        }
+        if (empty())
+        {
 
-        T operator[](int index) {
-            // TODO
+            newNode = new Node<T>{value, nullptr, nullptr};
+            this->head = newNode;
+            this->tail = newNode;
         }
+        else
+        {
+            newNode = new Node<T>{value, nullptr, this->tail};
+            this->tail->next = newNode;
+            this->tail = newNode;
+        }
+        this->nodes++;
+    }
 
-        bool empty() {
-            // TODO
-        }
+    void print()
+    {
+        Node<T> *current;
 
-        int size() {
-            // TODO
+        if (empty())
+        {
+            std::cout << "empty\n";
         }
+        else
+        {
+            current = this->head;
+            while (current != nullptr)
+            {
+                std::cout << current->data << '\n';
+                current = current->next;
+            }
+        }
+    }
 
-        void clear() {
-            // TODO
-        }
+    void print_reverse()
+    {
+        Node<T> *current;
 
-        void sort() {
-            // TODO
+        if (empty())
+        {
+            std::cout << "empty\n";
         }
-    
-        void reverse() {
-            // TODO
+        else
+        {
+            current = this->tail;
+            while (current != nullptr)
+            {
+                std::cout << current->data << '\n';
+                current = current->prev;
+            }
         }
+    }
 
-        string name() {
-            return "Forward List";
-        }
 
-        ForwardIterator<T> begin() {
-            // TODO
+    void pop_front()
+    {
+        if (!empty())
+        {
+            if (this->head == this->tail)
+            {
+                delete this->head;
+                this->head = nullptr;
+                this->tail = nullptr;
+            }
+            else
+            {
+                this->head = this->head->next;
+                delete this->head->prev;
+                this->head->prev = nullptr;
+            }
         }
+        this->nodes--;
+    }
 
-	    ForwardIterator<T> end() {
-            // TODO
+    void pop_back()
+    {
+        if (!empty())
+        {
+            if (this->head == this->tail)
+            {
+                delete this->head;
+                this->head = nullptr;
+                this->tail = nullptr;
+            }
+            else
+            {
+                this->tail = this->tail->prev;
+                delete this->tail->next;
+                this->tail->next = nullptr;
+            }
         }
+        this->nodes--;
+    }
 
-        void merge(ForwardList<T> list) {
-            // TODO
+    T operator[](int index)
+    {
+        Node<T> *current;
+        if (this->nodes == 0)
+        {
+            throw std::invalid_argument("List is empty.\n");
         }
+        else if (this->nodes > index && index >= 0)
+        {
+            current = this->head;
+            int i = 0;
+            while (i < index)
+            {
+                current = current->next;
+                ++i;
+            }
+            return current->data;
+        }
+        else
+        {
+            throw std::invalid_argument("Index out of range.\n");
+        }
+    }
+
+    bool empty()
+    {
+        return this->head == nullptr ? true : false;
+    }
+
+    int size()
+    {
+        return (this->nodes);
+    }
+
+    void clear()
+    {
+        while(!empty())
+        {
+            pop_back();
+        }
+    }
+
+    void sort()
+    {
+        // TODO
+    }
+
+    void reverse()
+    {
+        // TODO
+    }
+
+    std::string name()
+    {
+        return "Forward List";
+    }
+
+    ForwardIterator<T> begin()
+    {
+        // TODO
+    }
+
+    ForwardIterator<T> end()
+    {
+        // TODO
+    }
+
+    void merge(ForwardList<T> list)
+    {
+        if (empty())
+        {
+            if (!list.empty())
+            {
+                this->head = list.head;
+                this->tail = list.tail;
+            }
+        }
+        else
+        {
+            if (!list.empty())
+            {
+                this->tail->next = list.head;
+                list.head->prev = this->tail;
+                this->tail = list.tail;
+            }
+        }
+            }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include "test/tester.h"
+#include "forward.h"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     cout << "===========================================================" << endl;
     cout << "\tLists Practice" << endl;
-    cout << "===========================================================" << endl << endl;
+    cout << "===========================================================" << endl
+         << endl;
 
-    Tester::execute();
+    // Tester::execute();
+    ForwardList<int> l;
+    ForwardList<int> l1;
+    l.push_back(1);
+    l.push_back(2);
+
+    l1.push_back(3);
+    l1.push_back(4);
+    l.merge(l1);
+    l.print();
+    l.print_reverse();
     return EXIT_SUCCESS;
 }
